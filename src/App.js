b69f57f3d609711ad32a6f18c5f974744c269b0a
Imran Sah <Imran.Sah@thesmartq.com>
2022-01-03 14:54:16 +0530

    background

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import "./App.css";
 import { Routes, Route, useNavigate } from "react-router-dom";
+import { useEffect, useLayoutEffect, useState } from "react";
+import "./assets/Mask Group 139.svg";
 
 function App() {
-	return <Routes></Routes>;
+	const [width, height] = useWindowSize();
+	return width <= 450 && height <= 850 ? (
+		<div className="App">
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<h4>
+							Window size: {width} x {height}
+						</h4>
+					}
+				/>
+			</Routes>
+		</div>
+	) : (
+		<h4>change to mobile view</h4>
+	);
+}
+
+function useWindowSize() {
+	const [size, setSize] = useState([0, 0]);
+	useLayoutEffect(() => {
+		function updateSize() {
+			setSize([window.innerWidth, window.innerHeight]);
+		}
+		window.addEventListener("resize", updateSize);
+		updateSize();
+		return () => window.removeEventListener("resize", updateSize);
+	}, []);
+	return size;
 }
 
 export default App;
